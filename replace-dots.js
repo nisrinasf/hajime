04661b83ce635804e160b012653af6f4a8282c16
nisrinasf <111826125+nisrinasf@users.noreply.github.com>
2026-06-04 20:06:33 +0700

    Create replace-dots.js
    hari kedua

---
+++ replace-dots.js
@@ -0,0 +1,8 @@
+var replaceDots = function(str) {
+  return str.replace(/\./g, '-')
+}
+
+/**
+Special karakter dalam regex harus di beri backslash salah satunya adalah titik.
+function replace() mengganti dengan input yang diinginkan
+**/
