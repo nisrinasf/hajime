f0f1fdf167c0808adb0e889e4fafef06f0cad526
nisrinasf <111826125+nisrinasf@users.noreply.github.com>
2026-06-04 20:35:29 +0700

    Update lower-case-count.js

--- lower-case-count.js
+++ lower-case-count.js
@@ -8,4 +8,16 @@ function lowercaseCount(str){
   **/
   let res = str.match(/[a-z]/g)
   return  res == null ? 0 : res.length
+
+  // alternatif 
+  return (str.match(/[a-z]/g) || []).length
+  /**
+  Fungsi match mengembalikan null jika tidak ada kecocokan. 
+  Jadi, jika Anda menghapus koma || []dan melakukan lowercaseCount("QWE"), 
+  Anda akan mendapatkan kesalahan yang menyatakan tidak dapat membaca panjang null karena QWE tidak cocok dengan regex 
+  (semuanya huruf besar, dan regex cocok dengan huruf kecil).
+
+  Penambahan ini || []membuat array kosong akan dibuat jika fungsi pencocokan bernilai null,
+  dan karena array kosong memiliki properti panjang, Anda tidak akan mendapatkan kesalahan apa pun.
+  **/
 }
