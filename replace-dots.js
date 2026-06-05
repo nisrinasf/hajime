f975af6d2005b7d60fcddf6b122cd64359e966e8
nisrinasf <111826125+nisrinasf@users.noreply.github.com>
2026-06-06 01:59:33 +0700

    Delete replace-dots.js

--- replace-dots.js
+++
@@ -1,8 +0,0 @@
-var replaceDots = function(str) {
-  return str.replace(/\./g, '-')
-}
-
-/**
-Special karakter dalam regex harus di beri backslash salah satunya adalah titik.
-function replace() mengganti dengan input yang diinginkan
-**/
