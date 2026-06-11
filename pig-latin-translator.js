03c01a8a3df016e40aa2f93b4762f938475b511e
nisrinasf <111826125+nisrinasf@users.noreply.github.com>
2026-06-11 23:43:03 +0700

    Update pig-latin-translator.js

--- pig-latin-translator.js
+++ pig-latin-translator.js
@@ -4,4 +4,37 @@ var translate = function(word) {
   else return word.slice(1) + word[0] + 'ay' 
 }
 // bestpractice
-return word.replace(/^(?=..)([^aeiou]?)(.*)$/, '$2$1ay');
+word.length > 1 ? word.replace(/([^aeiou]?)(\w+)/, '$2$1ay') : word
+
+/**
+ * Penjelasn Group 1 ([^aiueo]?)
+ * [] = karakter set
+ * ^ di dalam [] = negasi (bukan)
+ * aeiou = huruf vokal
+ * JADI : berarti satu karakter yang bukan a, e, i, o, atau u.
+ * 
+ * Tanda ?
+ * JADI : artinya 0 atau 1 karakter konsonan.
+ * | Input | Group 1  |
+ * | ----- | -------- |
+ * | dog   | d        |
+ * | cat   | c        |
+ * | apple | (kosong) |
+ *
+ * Group 2 (\w+)
+ * huruf (a-z, A-Z)
+ * angka (0-9)
+ * underscore (_)
+ * 
+ * Tanda +
+ * JADI : berarti satu atau lebih. (menangkap sisa kata.)
+ * 
+ * Kata pengganti '$2$1ay'
+ * $1 = isi group 1
+ * $2 = isi group 2
+ * 
+ * JADI : group2 + group1 + "ay"
+ * 
+ * jika terdapat huruf konsonan 1 maka ganti dengan group 2 (sisa kata) + group 1 (huruf konsonan)
+ * jika terdapat huruf konsonan 0 (huruf vokal) maka ganti dengan group 2 (sisa kata) + group 1 (koson)
+ */
