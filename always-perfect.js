b7b2dd5768345d728edc81711f7b313147d84881
nisrinasf <111826125+nisrinasf@users.noreply.github.com>
2026-06-14 19:14:42 +0700

    Update always-perfect.js

--- always-perfect.js
+++ always-perfect.js
@@ -10,3 +10,13 @@ function checkRoot(string){
   }else{
       return  'incorrect input'  
   }      
+
+//alternatif (part some)
+function checkRoot(string) {
+  var parts = string.split(",")
+  if (parts.length != 4 || parts.some(isNaN)) return "incorrect input"
+  if (parts.some((x, i) => i > 0 && x - parts[i - 1] != 1)) return "not consecutive"
+  var n = parts.reduce((n, x) => n * x, 1) + 1
+  var p = Math.sqrt(n)
+  return `${n}, ${p}`
+}
