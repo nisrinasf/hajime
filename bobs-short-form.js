7305dd2269456510edda833be3df32d17cdfb05f
nisrinasf <111826125+nisrinasf@users.noreply.github.com>
2026-06-20 09:24:07 +0700

    Create bobs-short-form.js

---
+++ bobs-short-form.js
@@ -0,0 +1,8 @@
+function shortForm(str) {
+  return str.replace(/(^[aiueo]?)|([aiueo]?$)|([aiueo])/gi, '$1$2')
+}
+// hapus huruf vokal selain di depan dan belakang
+console.log(shortForm("amazing")) //amazng
+console.log(shortForm("assauolta")) // asslta
+console.log(shortForm("thypodoha")) //thypdha
+console.log(shortForm("aitaai")) // ati
