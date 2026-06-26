b93a142d585ea1b7704c359d527ed76bea2315a4
nisrinasf <111826125+nisrinasf@users.noreply.github.com>
2026-06-26 19:20:50 +0700

    Create convert-number-to-money.js

---
+++ convert-number-to-money.js
@@ -0,0 +1,30 @@
+var numberToMoney = function(n) {
+  let res;
+   n.toString().split(/\./).map((money, index, arr) => {
+    if(index == 0){
+      res = money
+          .split('').reverse().join('')
+          .match(/\d{0,3}/g).reverse()
+          .map(e => e.length > 1 ? e.split('').reverse().join('') : e)
+          .filter(e => e)
+          .join(',')
+    }
+    
+    else{
+      digit = money.slice(0,2).match(/([0-9]?)([^0])/g) || []
+      res = digit.length > 0 ? res + '.' + digit.join('') : res 
+    }
+  })
+
+  return res
+ 
+};
+
+
+console.log(numberToMoney(1.0187987)) // 2,546.25
+console.log(numberToMoney(1.1067497241248292)) // 2,546.25
+console.log(numberToMoney(1.1)) // 2,546.25
+console.log(numberToMoney(1.187987)) // 2,546.25
+console.log(numberToMoney(1787.187987)) // 2,546.25
+console.log(numberToMoney(1.008888)) // 2,546.25
+
