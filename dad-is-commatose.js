be3070dea8af75f2d6b9f6368dc85a2c94d0813a
hikachuuuxd <hikarimizu1111@gmail.com>
2026-07-03 00:41:46 +0700

    mmenambah alternatif jawaban -96 kyu7-regex

--- dad-is-commatose.js
+++ dad-is-commatose.js
@@ -5,3 +5,21 @@ return str.replace(/,+/g, ',').replace(/,\s?$/g, '')
 console.log(dadFilter("all this,,,, used to be trees,,,,,,"))
 console.log(dadFilter("i,, don't believe this round earth,,, show me evadence!!"))
 console.log(dadFilter("ahsflsdhldsk,,,, skdfjlsdlkfjsdfj,, "))
+
+// alternatif
+
+function dadFilter(str) {
+  return str.replace(/\B,|[, ]+$/g, '');
+}
+
+/**
+ * \B, = bukan permulaan kata dari koma
+ * contoh = "all this,,,, used to be trees"
+ * match = semua tanda koma selain satu koma yang paling akhir [,,,]
+ * 
+ * [, ] = semua tanda koma yang memiliki whitespace atau tidak lebih dari 1 di akhir kalimat
+ * consoh = "contoh = all this,,,, used to be trees,,, "
+ * consoh2 = "contoh = all this,,,, used to be trees,,,"
+ * match = [,,, ]
+ * match2 = [,,,]
+ */
