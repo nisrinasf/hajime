66f3dd7188ff374d226b1610cba41587b74a18bf
hikachuuuxd <hikarimizu1111@gmail.com>
2026-07-04 00:04:53 +0700

    -86 kyu7-regex

--- format-of-a-ticket-number.js
+++ format-of-a-ticket-number.js
@@ -11,7 +11,7 @@ function nbrValidTickets(tickets){
  return tickets.filter(x => /^[a-z]\d[a-z]{4}$/i.test(x)).length;
 }
 
-console.log(nbrValidTickets( ["A9JZOD","E9FIDH","SI2JIS","F8JIODJ","FDSNJA","A9POF?","AA9DIJD"])) // valid tiket number = 2
+console.log(nbrValidTickets( ["A9JZOD","E9FIDH","SI2JIS","F8JIODJ","FDSNJA","A9POF?","AA9DIJD"])) // total valid tiket number = 2
 /**
 * Berisi tepat 6 karakter
 * Berisi: satu huruf kemudian satu angka kemudian 4 huruf
