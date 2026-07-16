d91cf014092d25e74845d5c9b98a7e3f5f365afa
hikachuuuxd <hikarimizu1111@gmail.com>
2026-07-16 22:53:52 +0700

    toFixed()

---
+++ Numbers/dollars-and-cents.js
@@ -0,0 +1,9 @@
+function formatMoney(amount){
+  return amount.toFixed(2);
+}
+
+console.log(formatMoney(3))
+console.log(formatMoney(3.1))
+console.log(formatMoney(8.8))
+console.log(formatMoney(3.11))
+console.log(formatMoney(39.11))
